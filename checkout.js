9f28a25d7b5801a927423ded09f6bf7682d3c7bf
hasslegnend <hasslegend294@gmail.com>
2026-07-28 12:05:18 +1000

    worked on the checkout

--- checkout.js
+++ checkout.js
@@ -1,2 +1,14 @@
-console.log("The script is successfully linked!");
+document.getElementById("Pay").addEventListener("submit", function(e){
+    e.preventDefault();
 
+    const email = document.getElementById("email").value;
+    const selectedGames = Array.from(document.querySelectorAll('input[name="games"]:checked')).map(el => el.value);
+
+    if (selectedGames.length === 0) {
+        alert("No Items Selected To Checkout!.");
+        return;
+    }
+
+   window.location.href = "thankyou.html"
+});
+s
